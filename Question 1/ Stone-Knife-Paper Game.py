c597e2dd23344c29bccc19288f52942ac6e89fb0
Jaymin Amin <jayminamin06@gmail.com>
2026-04-14 19:40:54 +0300

    Corrections

--- Question 1/ Stone-Knife-Paper Game.py
+++ Question 1/ Stone-Knife-Paper Game.py
@@ -3,9 +3,9 @@ import random
 human = input("Input S for stone,K for knife and P for paper: ").capitalize()
 computer = random.randint(0,2)
 
-if human == "K" and computer == 1:
+if human == "K" and computer == 0:
     print("Stone blunts Knife. Computer wins!!")
-elif human == "S" and computer == 0:
+elif human == "S" and computer == 1:
     print("Stone blunts Knife. Player wins!!")
 elif human == "K" and computer == 2:
     print("Knife cuts paper. Player wins!!")
@@ -17,3 +17,5 @@ elif human == "S" and computer == 2:
     print("Paper covers stone. Computer wins")
 elif (human == "S" and computer == 0) or (human == "K" and computer == 1) or (human == "P" and computer == 2):
     print("draw!!!. no one wins!!")
+else:
+    print("Invalid input! Please enter S, K, or P.")
